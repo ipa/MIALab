ee9b9a4ddf2e53855e7e94b0d9e68c2695a8eee0
Severin Tobler <tobler.severin@gmail.com>
2015-12-12 17:39:05 +0100

    added dice computation

--- src/project/postprocessPredictionVisual.m
+++ src/project/postprocessPredictionVisual.m
@@ -1,38 +1,43 @@
-function [ Pm ] = postprocessPredictionVisual( Ps )
+function [ Pm ] = postprocessPredictionVisual( Ps , Label, path )
 %POSTPROCESSPREDICTION Summary of this function goes here
 %   Detailed explanation goes here
 
 voxelSize=[0.3906 0.3906 1];
-slide = 40;
 
 T = 0.5;
 Pm = Ps > T;
 
+dices = zeros(5,1);
+
+dices(1) = dice(Pm, Label);
 figure;
 visualizeVolume(Pm,Ps,voxelSize);
-print(gcf, '../../doc/results/beforePostprocessing', '-depsc');
+print(gcf, [path 'beforePostprocessing'], '-depsc');
 
 for k = 1:size(Pm, 3)
     Pm(:,:,k) = imopen(Pm(:,:,k), strel('disk', 3));
 end
 
+dices(2) = dice(Pm, Label);
 visualizeVolume(Pm,Ps,voxelSize);
-print(gcf, '../../doc/results/afterOpening', '-depsc');
+print(gcf, [path 'afterOpening'], '-depsc');
 
 for k = 1:size(Pm, 3)
     Pm(:,:,k) = keepLargestArea(Pm(:,:,k));
 end
 
+dices(3) = dice(Pm, Label);
 visualizeVolume(Pm,Ps,voxelSize);
-print(gcf, '../../doc/results/afterKeepLargestArea', '-depsc');
+print(gcf, [path 'afterKeepLargestArea'], '-depsc');
 
 
 for k = 1:size(Pm, 3)
     Pm(:,:,k) = imfill(Pm(:,:,k), 'holes');
 end
 
+dices(4) = dice(Pm, Label);
 visualizeVolume(Pm,Ps,voxelSize);
-print(gcf, '../../doc/results/afterImFill', '-depsc');
+print(gcf, [path 'afterImFill'], '-depsc');
 
 
 labeledImage = bwlabeln(Pm, 6);
@@ -41,11 +46,12 @@ stat = regionprops(labeledImage, 'area');
 biggestBlob = ismember(labeledImage, sortIndexes(1));
 Pm = biggestBlob > 0;
 
+dices(5) = dice(Pm, Label);
 visualizeVolume(Pm,Ps,voxelSize);
-print(gcf, '../../doc/results/afterKeepLargestVolume', '-depsc');
+print(gcf, [path 'afterKeepLargestVolume'], '-depsc');
 title('afterKeepLargestVolume3D');
 
-% close all;
+save([path 'dices.m', 'dices']);
 
 end
 
